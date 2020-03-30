8edb24b56fef64aa54449196bbc3684eeb1a9ed4
simodevp <mohamed.benhassi9@gmail.com>
2020-03-30 17:44:03 +0100

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Simo on 3/30/20.
+//  Copyright © 2020 A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
